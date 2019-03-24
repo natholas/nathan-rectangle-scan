3b732d03ef0772c5bf179c5cb6739dc247ff581e
Nathan Felix <natholas@gmail.com>
2019-03-24 08:33:24 +0100

    Added more sliders in the demo

--- demo/demo.js
+++ demo/demo.js
@@ -4,7 +4,16 @@ const image = document.querySelector("#test-image")
 const checker = new RectCheck()
 
 function vChange (e) {
-  imageOverlay.style[e.target.classList[0]] = e.target.value + 'px';
+  document.querySelector('#' + e.target.id + 'Val').innerHTML = e.target.value
+  if (e.target.id === 'edgeEl') {
+    checker.ops.edgeThreshhold = parseInt(e.target.value)
+  } else if (e.target.id === 'contrastEl') {
+    checker.ops.minContrast = parseInt(e.target.value)
+  } else if (e.target.id === 'searchWidthEl') {
+    checker.ops.searchWidth = parseInt(e.target.value)
+  } else {
+    imageOverlay.style[e.target.classList[0]] = e.target.value + 'px';
+  }
   scanImage()
 }
 
@@ -17,6 +26,9 @@ document.querySelector('#topEl').addEventListener('input', vChange)
 document.querySelector('#leftEl').addEventListener('input', vChange)
 document.querySelector('#widthEl').addEventListener('input', vChange)
 document.querySelector('#heightEl').addEventListener('input', vChange)
+document.querySelector('#edgeEl').addEventListener('input', vChange)
+document.querySelector('#contrastEl').addEventListener('input', vChange)
+document.querySelector('#searchWidthEl').addEventListener('input', vChange)
 
 const scanImage = () => {
   const bounds = imageOverlay.getBoundingClientRect()
